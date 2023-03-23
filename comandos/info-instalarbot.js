463e85c271fb7207b84721f6c8a0b16dd7da7736
Clash538 <118075131+Clash538@users.noreply.github.com>
2023-03-23 03:39:38 -0400

    DARK-BOT-OFC

---
+++ comandos/info-instalarbot.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+let text = `
+
+*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+> cd
+> termux-setup-storage
+> apt update 
+> pkg upgrade 
+> pkg install git -y
+> pkg install nodejs -y
+> pkg install ffmpeg -y
+> pkg install imagemagick -y
+> pkg install yarn
+> git clone https://github.com/Clash538/DARK-BOT.git
+> cd DARK-BOT
+> yarn install 
+> npm install
+> npm update
+> npm install 
+> npm start
+`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃',
+body: '༒𝕯𐊅ℜ𝖐-𝕭𝖔𝖙⍉ꤲ',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `https://github.com/Clash538/DARK-BOT`}}})   
+}
+handler.command = /^(instalarbot)/i
+export default handler
