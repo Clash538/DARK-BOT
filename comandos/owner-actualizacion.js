3d3d0519d684c9b7f3ba97eeb2452b06f16cbc72
Clash538 <118075131+Clash538@users.noreply.github.com>
2023-03-23 13:38:20 -0400

    Update owner-actualizacion.js

--- comandos/owner-actualizacion.js
+++ comandos/owner-actualizacion.js
@@ -1,16 +1,16 @@
 import fs from 'fs'
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-const grupo = 'https://chat.whatsapp.com/Gt8tzfxKFl15bqIAjxmqIe'
+const grupo = 'https://chat.whatsapp.com/LGukKTl7A8FDCzEWwerVlD'
 
 let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
 let users = m.sender.split`@`[0]
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let [_, code] = grupo.match(linkRegex) || []
 
-if ( users == 584125778026 || users == 584125778026 ) try {
+if ( users == 584129763459 || users == 584129763459 ) try {
 if (!text) return m.reply(`*Falta Texto*`) 
 let res = await conn.groupAcceptInvite(code)
-await conn.sendMessage(res, { text: text + '\n\n_atte. 𝐃𝐈𝐄𝐆𝐎-𝐎𝐅𝐂_', mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res, { text: text + '\n\n_atte. 𝕮𝖆𝖗𝖑𝖔𝖘-𝖔𝖋𝖈_', mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `)
 
 } catch (e) {
